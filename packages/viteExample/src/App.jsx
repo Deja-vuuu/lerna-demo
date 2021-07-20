ac609966c886983410390fc1e609a79bf9c3e7e6
杨阳 <1002522146@qq.com>
2021-07-20 23:46:34 +0800

    :tada: (init) init

---
+++ packages/viteExample/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { LuckyWheel } from 'react-luck-draw'
+
+function App() {
+  return (
+    <div className="App">
+   
+   <LuckyWheel width="300px" height="300px"></LuckyWheel>
+    </div>
+  )
+}
+
+export default App
